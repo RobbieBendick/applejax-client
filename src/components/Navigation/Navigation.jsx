4b617831d8fbb0c25c828e01076a9c14206b0f56
Robbie Bendick <robbiebendick@gmail.com>
2021-12-12 14:26:15 -0800

    mobile nav

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,25 +1,25 @@
-import "./Navigation.css"
+import "./Navigation.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap"
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 function Navigation() {
     return (
-        <div>
-            <Navbar bg="navColor" variant="dark">
+            <Navbar bg="navColor" variant="dark" sticky="top" expand="xl" collapseOnSelect>
                 <Navbar.Brand style={{"padding": "10px 20px"}}>
                     Rob
                 </Navbar.Brand>
-                <Nav>
-                    <NavDropdown title="Robdog">
-                        <NavDropdown.Item href="/rob">rob</NavDropdown.Item>
-                        <NavDropdown.Item href="/dog">dog</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="/robdog">robdog</NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Nav>
+                        <NavDropdown title="Robdog">
+                            <NavDropdown.Item href="/rob">rob</NavDropdown.Item>
+                            <NavDropdown.Item href="/dog">dog</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="/robdog">robdog</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
             </Navbar>
-
-        </div>
     )
 }
 

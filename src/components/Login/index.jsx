aa2728fae2deb7211386f70f10543139a8230256
payman <paypay@paymans-air.mynetworksettings.com>
2021-12-22 05:28:51 -0500

    Changed title and added helmet for title

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,10 +1,11 @@
-import  * as React from 'react';
-import {useState} from "react";
-import {FormControl} from 'baseui/form-control';
-import {Input} from 'baseui/input';
-import {useStyletron} from 'baseui';
-import {Alert} from 'baseui/icon';
-import {Button} from 'baseui/button';
+import * as React from "react";
+import { useState } from "react";
+import { FormControl } from "baseui/form-control";
+import { Input } from "baseui/input";
+import { useStyletron } from "baseui";
+import { Alert } from "baseui/icon";
+import { Button } from "baseui/button";
+import { Helmet } from "react-helmet";
 import axios from "axios";
 
 function Negative() {
@@ -12,8 +13,8 @@ function Negative() {
   return (
     <div
       className={css({
-        display: 'flex',
-        alignItems: 'center',
+        display: "flex",
+        alignItems: "center",
         paddingRight: theme.sizing.scale500,
         color: theme.colors.negative400,
       })}
@@ -24,20 +25,20 @@ function Negative() {
 }
 
 function Login() {
-    const [values, setValues] = useState({
-        username: "",
-        password: ""
-    });
+  const [values, setValues] = useState({
+    username: "",
+    password: "",
+  });
 
-    const [isValid, setIsValid] = useState({
-        username: false,
-        password: false,
-    })
-    const [isVisited, setIsVisited] = useState({
-        email: false,
-        username: false,
-        password: false,
-    })
+  const [isValid, setIsValid] = useState({
+    username: false,
+    password: false,
+  });
+  const [isVisited, setIsVisited] = useState({
+    email: false,
+    username: false,
+    password: false,
+  });
   const [css, theme] = useStyletron();
 
   // function loginAPICall() {
@@ -49,48 +50,52 @@ function Login() {
   // }
 
   return (
-        <>
-        <form
-            className={css({
-            width: "50%",
-            margin: "0 auto",
-            paddingTop: "35px"
-        })} 
-        onSubmit={e => {
+    <>
+      <Helmet>
+        {" "}
+        <title>Login - Applejax</title>
+      </Helmet>
+      <form
+        className={css({
+          width: "50%",
+          margin: "0 auto",
+          paddingTop: "35px",
+        })}
+        onSubmit={(e) => {
           e.preventDefault();
           // loginAPICall();
-        }}>
-        <FormControl
-            label="Your username"
-        >
-            <Input
+        }}
+      >
+        <FormControl label="Your username">
+          <Input
             placeholder="Enter your username"
             id="username-input-id"
             value={values.username}
-            onChange={e => setValues({...values, username: e.currentTarget.value})}
+            onChange={(e) =>
+              setValues({ ...values, username: e.currentTarget.value })
+            }
             onBlur={() => setIsVisited({ ...isVisited, username: true })}
             type="username"
             required
-            />
+          />
         </FormControl>
-        <FormControl
-            label="Your password"
-        >
-            <Input
+        <FormControl label="Your password">
+          <Input
             placeholder="Enter your password"
             id="password-input-id"
             value={values.password}
-            onChange={e => setValues({...values, password: e.currentTarget.value})}
-            onBlur={() => setIsVisited({...isVisited, password: true})}
+            onChange={(e) =>
+              setValues({ ...values, password: e.currentTarget.value })
+            }
+            onBlur={() => setIsVisited({ ...isVisited, password: true })}
             type="password"
             autoComplete="current-password"
             required
-            />
+          />
         </FormControl>
         <Button type="submit">Login</Button>
-        </form>
-        </>
-    
+      </form>
+    </>
   );
 }
 
--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,19 +1,21 @@
-import  * as React from 'react';
-import {useState} from "react";
-import {FormControl} from 'baseui/form-control';
-import {Input} from 'baseui/input';
-import {useStyletron} from 'baseui';
-import {Alert} from 'baseui/icon';
-import {Button} from 'baseui/button';
-import {validate as validateEmail} from 'email-validator';
-import axios from 'axios';
+import * as React from "react";
+import { useState } from "react";
+import { FormControl } from "baseui/form-control";
+import { Input } from "baseui/input";
+import { useStyletron } from "baseui";
+import { Alert } from "baseui/icon";
+import { Button } from "baseui/button";
+import { validate as validateEmail } from "email-validator";
+import axios from "axios";
+import { Helmet } from "react-helmet";
+
 function Negative() {
   const [css, theme] = useStyletron();
   return (
     <div
       className={css({
-        display: 'flex',
-        alignItems: 'center',
+        display: "flex",
+        alignItems: "center",
         paddingRight: theme.sizing.scale500,
         color: theme.colors.negative400,
       })}
@@ -24,103 +26,137 @@ function Negative() {
 }
 
 function Register() {
-    const [formStateHolder, setFormStateHolder] = useState({
-        email: {
-            isValid: false,
-            isVisited: false,
-            value: "",
-        },
-        username: {
-            isValid: false,
-            isVisited: false,
-            value: "",
-        },
-        password: {
-            isValid: false,
-            isVisited: false,
-            value: "",
-        }
-    });
-  const {email, username, password} = formStateHolder;
-  const shouldShowErrorEmail = (!email.isValid && email.isVisited) && email.value.length > 0;
+  const [formStateHolder, setFormStateHolder] = useState({
+    email: {
+      isValid: false,
+      isVisited: false,
+      value: "",
+    },
+    username: {
+      isValid: false,
+      isVisited: false,
+      value: "",
+    },
+    password: {
+      isValid: false,
+      isVisited: false,
+      value: "",
+    },
+  });
+  const { email, username, password } = formStateHolder;
+  const shouldShowErrorEmail =
+    !email.isValid && email.isVisited && email.value.length > 0;
   const [css, theme] = useStyletron();
 
   function registerApiCall() {
-    const {email, username, password} = formStateHolder;
-    axios.post("http://localhost:9000/register", {
+    const { email, username, password } = formStateHolder;
+    axios
+      .post("http://localhost:9000/register", {
         email: email.value,
         username: username.value,
-        password: password.value
-    }).then(response => console.log(response))
-    .catch(err => {
+        password: password.value,
+      })
+      .then((response) => console.log(response))
+      .catch((err) => {
         console.log(err);
-    });
+      });
   }
 
   return (
-        <>
-        <form className={css({
-            width: "50%",
-            margin: "0 auto",
-            paddingTop: "35px"
+    <>
+      <Helmet>
+        {" "}
+        <title>Register - Applejax</title>
+      </Helmet>
+      <form
+        className={css({
+          width: "50%",
+          margin: "0 auto",
+          paddingTop: "35px",
         })}
-            onSubmit={
-                e => {
-                    e.preventDefault();
-                    registerApiCall();
-                }
-            }>
-            <FormControl
-                label="Your email"
-                error={
-                shouldShowErrorEmail
-                    ? 'Please input a valid email address'
-                    : null
-                }
-            >
-                <Input
-                placeholder="Enter your email"
-                id="email-input-id"
-                value={email.value}
-                onChange={e => setFormStateHolder({...formStateHolder, email: {...email, value: e.currentTarget.value, isValid: validateEmail(e.currentTarget.value)}})}
-                onBlur={() => setFormStateHolder({...formStateHolder, email: {...email, isVisited: true}})}
-                error={shouldShowErrorEmail}
-                overrides={shouldShowErrorEmail ? {After: Negative} : {}}
-                type="email"
-                required
-                />
-            </FormControl>
-            <FormControl
-                label="Your username"
-            >
-                <Input
-                placeholder="Enter your username"
-                id="username-input-id"
-                value={username.value}
-                onChange={e => setFormStateHolder({...formStateHolder, username: {...username, value: e.currentTarget.value}})}
-                onBlur={() => setFormStateHolder({...formStateHolder, username: {...username, isVisited: true}})}
-                type="username"
-                required
-                />
-            </FormControl>
-            <FormControl
-                label="Your password"
-            >
-                <Input
-                placeholder="Enter your password"
-                id="password-input-id"
-                value={password.value}
-                onChange={e => setFormStateHolder({...formStateHolder, password: {...password, value: e.currentTarget.value}})}
-                onBlur={() => setFormStateHolder({...formStateHolder, password: {...password, isVisited: true}})}
-                type="password"
-                autoComplete="current-password"
-                required
-                />
-            </FormControl>
+        onSubmit={(e) => {
+          e.preventDefault();
+          registerApiCall();
+        }}
+      >
+        <FormControl
+          label="Your email"
+          error={
+            shouldShowErrorEmail ? "Please input a valid email address" : null
+          }
+        >
+          <Input
+            placeholder="Enter your email"
+            id="email-input-id"
+            value={email.value}
+            onChange={(e) =>
+              setFormStateHolder({
+                ...formStateHolder,
+                email: {
+                  ...email,
+                  value: e.currentTarget.value,
+                  isValid: validateEmail(e.currentTarget.value),
+                },
+              })
+            }
+            onBlur={() =>
+              setFormStateHolder({
+                ...formStateHolder,
+                email: { ...email, isVisited: true },
+              })
+            }
+            error={shouldShowErrorEmail}
+            overrides={shouldShowErrorEmail ? { After: Negative } : {}}
+            type="email"
+            required
+          />
+        </FormControl>
+        <FormControl label="Your username">
+          <Input
+            placeholder="Enter your username"
+            id="username-input-id"
+            value={username.value}
+            onChange={(e) =>
+              setFormStateHolder({
+                ...formStateHolder,
+                username: { ...username, value: e.currentTarget.value },
+              })
+            }
+            onBlur={() =>
+              setFormStateHolder({
+                ...formStateHolder,
+                username: { ...username, isVisited: true },
+              })
+            }
+            type="username"
+            required
+          />
+        </FormControl>
+        <FormControl label="Your password">
+          <Input
+            placeholder="Enter your password"
+            id="password-input-id"
+            value={password.value}
+            onChange={(e) =>
+              setFormStateHolder({
+                ...formStateHolder,
+                password: { ...password, value: e.currentTarget.value },
+              })
+            }
+            onBlur={() =>
+              setFormStateHolder({
+                ...formStateHolder,
+                password: { ...password, isVisited: true },
+              })
+            }
+            type="password"
+            autoComplete="current-password"
+            required
+          />
+        </FormControl>
         <Button type="submit">Register</Button>
-        </form>
-        </>
-    
+      </form>
+    </>
   );
 }
 
